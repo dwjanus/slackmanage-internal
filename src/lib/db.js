ff023d76a884e5b1cac00d85c67c2fba1ae204b3
Devin Janus <devinjanus@DJanus-MBA-8GB.local>
2017-01-16 14:32:46 -0500

    didnt work. attempting to populate requester via sfid push

--- src/lib/db.js
+++ src/lib/db.js
@@ -31,55 +31,56 @@ function retrieveCase (sfid, cb) {
   })
 }
 
-module.exports.createCase = (subject, user, description, cb) => {
-  let recordtypeid = '01239000000EB4NAAW'
-  let createQuery = 'INSERT INTO salesforcesandbox.case(subject, ' +
-    'creatorname, samanageesd__creatorname__c, samanageesd__requestername__c, ' +
-    'description, recordtypeid, samanageesd__recordtype__c, origin) ' +
-    'values($1, $2, $3, $4, $5, $6, $7, $8);'
-  let args = [subject, user, user, user, description, recordtypeid, 'Incident', 'Slack']
-  pool.query(createQuery, args)
-  console.log('~ Case created ~')
-  pool.connect().then(client => {
-    client.query('LISTEN status')
-    client.on('notification', data => {
-      console.log('-- notification fired, data.payload:\n', data.payload)
-      client.release()
-      console.log('-- client released, calling back results --')
-      retrieveCase(data.payload, cb)
-    })
-    .catch(err => {
-      client.release()
-      cb(err, null)
-    })
-  })
-}
-
 // module.exports.createCase = (subject, user, description, cb) => {
-//   pool.query(`SELECT Id FROM salesforcesandbox.users WHERE name ='${user}'`, (err, res) => {
-//     if (err) console.log(err)
-//     let userId = res.rows[0].id
-//     let recordtypeid = '01239000000EB4NAAW'
-//     let createQuery = 'INSERT INTO salesforcesandbox.case(subject, ' +
-//       'creatorname, samanageesd__creatorname__c, samanageesd__requesteruser__c, ' +
-//       'description, recordtypeid, samanageesd__recordtype__c, origin) ' +
-//       'values($1, $2, $3, $4, $5, $6, $7, $8);'
-//     let args = [subject, user, user, userId, description, recordtypeid, 'Incident', 'Slack']
-//     pool.query(createQuery, args)
-//     console.log('~ Case created ~')
-//     pool.connect().then(client => {
-//       client.query('LISTEN status')
-//       client.on('notification', data => {
-//         console.log('-- notification fired, data.payload:\n', data.payload)
-//         client.release()
-//         console.log('-- client released, calling back results --')
-//         retrieveCase(data.payload, cb)
-//       })
-//       .catch(err => {
-//         client.release()
-//         cb(err, null)
-//       })
+//   let recordtypeid = '01239000000EB4NAAW'
+//   let createQuery = 'INSERT INTO salesforcesandbox.case(subject, ' +
+//     'creatorname, samanageesd__creatorname__c, samanageesd__requestername__c, ' +
+//     'description, recordtypeid, samanageesd__recordtype__c, origin) ' +
+//     'values($1, $2, $3, $4, $5, $6, $7, $8);'
+//   let args = [subject, user, user, user, description, recordtypeid, 'Incident', 'Slack']
+//   pool.query(createQuery, args)
+//   console.log('~ Case created ~')
+//   pool.connect().then(client => {
+//     client.query('LISTEN status')
+//     client.on('notification', data => {
+//       console.log('-- notification fired, data.payload:\n', data.payload)
+//       client.release()
+//       console.log('-- client released, calling back results --')
+//       retrieveCase(data.payload, cb)
+//     })
+//     .catch(err => {
+//       client.release()
+//       cb(err, null)
 //     })
 //   })
 // }
+
+module.exports.createCase = (subject, user, description, cb) => {
+  pool.query(`SELECT Id FROM salesforcesandbox.users WHERE name ='${user}'`, (err, res) => {
+    if (err) console.log(err)
+    console.log('User search response:\n' + util.inspect(res.rows[0]))
+    let userId = res.rows[0].id
+    let recordtypeid = '01239000000EB4NAAW'
+    let createQuery = 'INSERT INTO salesforcesandbox.case(subject, ' +
+      'creatorname, samanageesd__creatorname__c, samanageesd__requesteruser__c, ' +
+      'description, recordtypeid, samanageesd__recordtype__c, origin) ' +
+      'values($1, $2, $3, $4, $5, $6, $7, $8);'
+    let args = [subject, user, user, userId, description, recordtypeid, 'Incident', 'Slack']
+    pool.query(createQuery, args)
+    console.log('~ Case created ~')
+    pool.connect().then(client => {
+      client.query('LISTEN status')
+      client.on('notification', data => {
+        console.log('-- notification fired, data.payload:\n', data.payload)
+        client.release()
+        console.log('-- client released, calling back results --')
+        retrieveCase(data.payload, cb)
+      })
+      .catch(err => {
+        client.release()
+        cb(err, null)
+      })
+    })
+  })
+}
 
