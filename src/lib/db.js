262fcd2103581602634b64e8dcf94f554d04f837
Devin Janus <devinjanus@DJanus-MBA-8GB.local>
2017-01-17 18:19:58 -0500

    moved even further in and trying to force t to look for notification

--- src/lib/db.js
+++ src/lib/db.js
@@ -48,30 +48,30 @@ module.exports.createCase = (subject, user, description) => {
       console.log(`~ 2. DB.task.then -> userId: ${util.inspect(userId.sfid)} ~`)
       let args = [subject, user, userId.sfid, description, recordtypeid, 'Incident', 'Slack']
       return t.none(createQuery, args)
-    })
-    .then(() => {
-      console.log('--> Done with tasks - awaiting listener')
-      console.log(`~ 3. DB.task.(second)then ~`)
-      let sco
-      return db.connect()
-      .then(obj => {
-        console.log(`~ 4. DB.connect.then ~`)
-        sco = obj
-        sco.client.on('notification', data => {
-          console.log('--> Recieved trigger data: ', data.payload)
-          return retrieveCase(data.payload)
+      .then(() => {
+        console.log('--> Done with tasks - awaiting listener')
+        console.log(`~ 3. DB.task.(second)then ~`)
+        // let sco
+        db.connect()
+        .then(obj => {
+          console.log(`~ 4. DB.connect.then ~`)
+          t = obj
+          t.client.on('notification', data => {
+            console.log('--> Recieved trigger data: ', data.payload)
+            return retrieveCase(data.payload)
+          })
+          return t.none('LISTEN status')
         })
-        return sco.none('LISTEN status')
-      })
-      .catch(err => {
-        console.log(err)
-      })
-      .finally(() => {
-        console.log(`~ 5. DB.connect.finally ~`)
-        if (sco) {
-          console.log(`~ 6. sco.done() ~`)
-          sco.done()
-        }
+        .catch(err => {
+          console.log(err)
+        })
+        .finally(() => {
+          console.log(`~ 5. DB.connect.finally ~`)
+          if (t) {
+            console.log(`~ 6. sco.done() ~`)
+            t.done()
+          }
+        })
       })
     })
   })
