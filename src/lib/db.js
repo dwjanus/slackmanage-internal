8af61d153eb91bb3955090f8a46e0feb8ce70a8e
Devin Janus <devinjanus@DJanus-MBA-8GB.local>
2017-01-17 17:41:07 -0500

    last commit

--- src/lib/db.js
+++ src/lib/db.js
@@ -77,10 +77,6 @@ module.exports.createCase = (subject, user, description) => {
   .catch(err => {
     console.log(err)
   })
-  .finally(retrievedCase => {
-    console.log(`~ finally -> retrievedCase:\n${util.inspect(retrievedCase)}`)
-    return retrievedCase
-  })
 }
 
 // import Pool from 'pg-pool'
