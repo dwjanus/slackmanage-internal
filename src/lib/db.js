68a437c7351bc5869c3676d32312c0ddac5144e0
Devin Janus <devinjanus@DJanus-MBA-8GB.local>
2017-01-20 12:25:38 -0500

    functionality basically restored. It responds again. Attempting to push creatorname field as well

--- src/lib/db.js
+++ src/lib/db.js
@@ -17,14 +17,15 @@ const pgConfig = {
   host: params.hostname,
   port: params.port,
   ssl: true,
+  poolSize: 20,
   poolIdleTimeout: 8000
 }
 const db = pgp(pgConfig)
 const recordtypeid = '01239000000EB4NAAW'
-const createQuery = 'INSERT INTO salesforce.case(subject, ' +
+const createQuery = 'INSERT INTO salesforce.case(subject, creatorname' +
       'samanageesd__creatorname__c, samanageesd__requesteruser__c, ' +
       'description, recordtypeid, samanageesd__recordtype__c, origin) ' +
-      'values($1, $2, $3, $4, $5, $6, $7)'
+      'values($1, $2, $3, $4, $5, $6, $7, $8)'
 
 // db.connect({direct: true})
 // .then(sco => {
@@ -66,7 +67,7 @@ module.exports.createCase = (subject, user, description) => {
     return t.one(`SELECT sfid FROM salesforce.user WHERE name = $1`, user)
     .then(userId => {
       console.log(`~ 2. DB.task.then -> userId: ${util.inspect(userId.sfid)} ~`)
-      let args = [subject, user, userId.sfid, description, recordtypeid, 'Incident', 'Slack']
+      let args = [subject, user, user, userId.sfid, description, recordtypeid, 'Incident', 'Slack']
       return t.none(createQuery, args)
       .then(() => {
         return retrieveCase().then(data => {
