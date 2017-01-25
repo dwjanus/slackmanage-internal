be0880f22743729b84b21881ec7d7d4c99638de1
Devin Janus <devinjanus@DJanus-MBA-8GB.local>
2017-01-25 16:32:56 -0500

    fixed typo

--- src/lib/conversation.js
+++ src/lib/conversation.js
@@ -1,6 +1,6 @@
 import _ from 'lodash'
 import util from 'util'
-import db from './ldb.js'
+import db from './db.js'
 // import ApiAi from '../config/middleware-apiai.js'
 
 export default (controller, bot) => {
